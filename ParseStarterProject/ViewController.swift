238e37493d95eac9feb1fe4c898c8078ef0f5c6e
rm <minowaryohei@minowa-no-MacBook-Air.local>
2015-06-25 10:37:17 +0900

    Count input characters

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -7,8 +7,11 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var remainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,11 +20,25 @@ class ViewController: UIViewController {
         testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
             println("Object has been saved.")
         }
+        
+        self.textView.layer.borderWidth = 1
+        self.textView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        let length = (textView.text as NSString).length + (text as NSString).length - range.length
+        
+        if length > 140 {
+            return false
+        }
+        remainLabel.text = "残り \(140 - length)"
+        return true
     }
 }
 
